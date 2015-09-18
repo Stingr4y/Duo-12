a58bbb0c746b139d8bcfb7d5037bbbfed0b79b3f
Stingr4y <miker281@hotmail.com>
2015-09-18 09:29:10 +0200

    added pdo.php

--- mysqli.php
+++ mysqli.php
@@ -6,24 +6,23 @@ $db = array (
     'dbname' => 'test' 
 ); 
 
-if(!mysql_connect($db['host'], $db['user'], $db['pass'])) 
+if(!mysqli_connect($db['host'], $db['user'], $db['pass'], $db['dbname'])) 
 { 
-    trigger_error('Fout bij verbinden: '.mysql_error()); 
+    trigger_error('Fout bij verbinden: '.mysqli_error()); 
 } 
-elseif(!mysql_select_db($db['dbname'])) 
+elseif(!mysqli_select_db($db['dbname'])) 
 { 
-    trigger_error('Fout bij selecteren database: '.mysql_error()); 
+    trigger_error('Fout bij selecteren database: '.mysqli_error()); 
 } 
 else 
 { 
     $sql = "SET SESSION sql_mode = 'ANSI,ONLY_FULL_GROUP_BY'"; 
-    if(!mysql_query($sql)) 
+    if(!mysqli_query($sql)) 
     { 
         trigger_error('MySQL in ANSI niet mogelijk'); 
     } 
 } 
 
 //echo uit DB
-
 
 ?>
